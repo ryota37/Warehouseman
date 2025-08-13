657c4c6608b45267906649bb39ce4e8f76f10cec
ryota37 <ryota.nakahama@access-company.com>
2025-08-13 09:03:05 +0900

    Impl: Player can push box

--- Warehouseman/Main.cpp
+++ Warehouseman/Main.cpp
@@ -32,12 +32,60 @@ public:
 	{
 	}
 
-	void update()
+	void update(Grid<CellState>& grid)
 	{
-		if(KeyUp.down()) gridY = std::max(0, gridY - 1);
-		if (KeyDown.down()) gridY = std::min(5, gridY + 1);
-		if (KeyLeft.down()) gridX = std::max(0, gridX - 1);
-		if (KeyRight.down()) gridX = std::min(7, gridX + 1);
+		if (KeyUp.down())
+		{
+			if (grid[gridY - 1][gridX].isWall)
+			{
+				return;
+			}
+			if (grid[gridY - 1][gridX].hasBox)
+			{
+				grid[gridY - 1][gridX].hasBox = false;
+				grid[gridY - 2][gridX].hasBox = true;
+			}
+			gridY = std::max(0, gridY - 1);
+		}
+		if (KeyDown.down())
+		{
+			if (grid[gridY + 1][gridX].isWall)
+			{
+				return;
+			}
+			if (grid[gridY + 1][gridX].hasBox)
+			{
+				grid[gridY + 1][gridX].hasBox = false;
+				grid[gridY + 2][gridX].hasBox = true;
+			}
+			gridY = std::min(5, gridY + 1);
+		}
+		if (KeyLeft.down())
+		{
+			if (grid[gridY][gridX - 1].isWall)
+			{
+				return;
+			}
+			if (grid[gridY][gridX - 1].hasBox)
+			{
+				grid[gridY][gridX - 1].hasBox = false;
+				grid[gridY][gridX - 2].hasBox = true;
+			}
+			gridX = std::max(0, gridX - 1);
+		}
+		if (KeyRight.down())
+		{
+			if (grid[gridY][gridX + 1].isWall)
+			{
+				return;
+			}
+			if (grid[gridY][gridX + 1].hasBox)
+			{
+				grid[gridY][gridX + 1].hasBox = false;
+				grid[gridY][gridX + 2].hasBox = true;
+			}
+			gridX = std::min(7, gridX + 1);
+		}
 	}
 
 private:
@@ -49,6 +97,16 @@ private:
 
 void InitializeWall(Grid<CellState>& grid)
 {
+	//for (int x = 0; x < grid.width(); ++x)
+	//{
+	//	grid[0][x].isWall = true; // Top wall
+	//	grid[grid.height() - 1][x].isWall = true; // Bottom wall
+	//}
+	//for (int y = 0; y < grid.height(); ++y)
+	//{
+	//	grid[y][0].isWall = true; // Left wall
+	//	grid[y][grid.width() - 1].isWall = true; // Right wall
+	//}
 }
 
 void InitializeBox(Grid<CellState>& grid)
@@ -105,6 +163,21 @@ void DrawGoal(Grid<CellState>& grid)
 	}
 }
 
+void DrawWall(Grid<CellState>& grid)
+{
+	for (int y = 0; y < grid.height(); ++y)
+	{
+		for (int x = 0; x < grid.width(); ++x)
+		{
+			if (grid[y][x].isWall)
+			{
+				RectF rect(Arg::center(convertGrid2Coordinate(x, y, 100)), 100, 100);
+				rect.draw(Palette::Brown);
+			}
+		}
+	}
+}
+
 void Main()
 {
 	Grid<CellState> grid(8, 6);
@@ -118,12 +191,13 @@ void Main()
 	while (System::Update())
 	{
 		// update
-		player.update();
+		player.update(grid);
 
 		// draw
 		DrawCell(grid);
 		DrawBox(grid);
 		DrawGoal(grid);
+		DrawWall(grid);
 		player.draw();
 
 		// Check for collisions
